255011a0ab76fd0dfc7212926970aaee4317de31
NicoR10 <36284539+NicoR10@users.noreply.github.com>
2020-11-09 20:37:11 -0300

    Add files via upload
    se mejoro el xpath, se reliza el filtrado del parse

--- cetrogar/cetrogar/spiders/cetrogar_spider.py
+++ cetrogar/cetrogar/spiders/cetrogar_spider.py
@@ -1,10 +1,15 @@
 import scrapy
+import re
 
 
 class CetrogarSpiderSpider(scrapy.Spider):
     name = 'cetrogar_spider'
     allowed_domains = ['www.cetrogar.com.ar']
     start_urls = ['https://www.cetrogar.com.ar/']
+    
+    def __init__(self, target='', **kwargs):
+        super().__init__(**kwargs)  # python3
+        self.target = target
 
     def parse(self, response):
         links = response.xpath('//div[@class="navigation-container"]/ul/li/a')
@@ -16,27 +21,23 @@ class CetrogarSpiderSpider(scrapy.Spider):
             
     def parse_productos(self, response):
         
+        #target = 'telefono'
+        
         for product in response.xpath("//ol[@class='products list items product-items  defer-images-grid']/li"):
-            yield {
-                
-                'title': product.xpath(".//a[@class='product-item-link']/text()").get(),
-                'price': product.xpath('.//span[@class="special-price"]//span[@class="price"]/text()').get()
-                
-            }
+            titulo = product.xpath(".//a[@class='product-item-link']/text()").get()
+            price = product.xpath('.//span[@class="price-container price-final_price tax weee"]/span[@data-price-type="finalPrice"]/span[@class="price"]/text()').get()
+            titulo = titulo.strip().lower()
+            
+            # Retorna solo si la palabra target esta en el titulo
+            if re.search(f"{self.target}+", titulo):
+                yield {
+                    
+                    'title': titulo,
+                    'price': price
+                    
+                }
         
         next_page = response.xpath('//a[@class="action  next"]/@href').get()
         
         if next_page:
             yield scrapy.Request(url=next_page, callback=self.parse_productos)
-        # productos = response.xpath('//a[@class="product-item-link"]/text()').getall()
-        # precios = response.xpath('//span[@class="special-price"]//span[@class="price"]/text()').getall()
-
-        # productos = [prod.strip() for prod in productos]
-
-        # yield {
-            
-        #     'productos': productos,
-        #     'precios': precios,
-        #     'cant_registros': len(productos)
-            
-        #     }
