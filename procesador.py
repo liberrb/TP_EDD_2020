388db1ffbca6a9996771e1872418b7e594bd9dbb
M-Correa <53838394+M-Correa@users.noreply.github.com>
2020-11-15 20:26:06 -0300

    Punto 2

--- procesador.py
+++ procesador.py
@@ -9,6 +9,19 @@ from json import JSONEncoder
 from config import Config
 import os
 import errno
+from difflib import SequenceMatcher as SM
+import difflib
+
+class ItemsAuxiliar():
+    def __init__(self,title,categoria,price,link,fecha,market,promedio,cantidad):
+        self.title = title
+        self.categoria = categoria
+        self.price = price
+        self.link = link
+        self.fecha = fecha
+        self.market = market
+        self.promedio = promedio
+        self.cantidad = cantidad
 
 
 class Procesador:
@@ -69,27 +82,106 @@ class Procesador:
             Html_file.write(html_str)
             Html_file.close()
 
-    def contarElementosLista(self,lista):
-        return {i:lista.count(i.get('title')) for i in lista}
+    def guardarTablaHtmlComparativa(self,lista,nombreItem):
+       
+        path = Config().get_path()
+
+        if not os.path.exists(os.path.dirname(path)):
+            try:
+                os.makedirs(os.path.dirname(path))
+            except OSError as exc:
+                if exc.errno != errno.EEXIST:
+                    raise
+
+        filename = path + nombreItem.replace(" ","_") +  "." + "html"
+
+        html_str = ""
+        if(nombreItem != "ComparativaOrdenada"):
+            html_str = """
+            <table border=1>
+                <tr>
+                <th>Title</th>
+                <th>Categoria</th>
+                <th>Price</th>
+                <th>Link</th>
+                <th>Fecha</th>
+                </tr>
+                <indent>""" 
+            for item in lista:
+                html_str +="""<tr>"""
+                html_str +="""<td>"""   + str(item.get('title')) + """</td>"""
+                html_str +="""<td>"""   + str(item.get('categoria')) + """</td>"""
+                html_str +="""<td>"""   + str(item.get('price')) + """</td>"""
+                html_str +="""<td>"""   + str(item.get('link')) + """</td>"""
+                html_str +="""<td>"""   + str(item.get('fecha')) + """</td>"""
+                html_str +="""</tr>"""
+        else:
+            html_str = """
+            <table border=1>
+                <tr>
+                <th>Title</th>
+                <th>Categoria</th>
+                <th>Price</th>
+                <th>Link</th>
+                <th>Fecha</th>
+                <th>Promedio</th>
+                <th>Cantidad</th>
+                </tr>
+                <indent>""" 
+            for item in lista:
+                html_str +="""<tr>"""
+                html_str +="""<td>"""   + str(item.title) + """</td>"""
+                html_str +="""<td>"""   + str(item.categoria) + """</td>"""
+                html_str +="""<td>"""   + str(item.price) + """</td>"""
+                html_str +="""<td>"""   + str(item.link) + """</td>"""
+                html_str +="""<td>"""   + str(item.fecha) + """</td>"""
+                html_str +="""<td>"""   + str(item.promedio) + """</td>"""
+                html_str +="""<td>"""   + str(item.cantidad) + """</td>"""
+                html_str +="""</tr>"""
+        
+        html_str +="""    </indent></table>"""
+        Html_file= open(filename,"w")
+        Html_file.write(html_str)
+        Html_file.close()
 
     def tablaComparativa(self,tipo,modelo =""):
         resultado = []
-
-
+        resultadoYaestan = []
         if(tipo == "a"):
             for item in self._miLista:
-                if(modelo in item.get('title')):
-                    resultado.append(item)
-            resultado.sort(key=lambda x: x.get('price'))
+                if(item.get('title') not in resultadoYaestan):
+                    for producto in self._miLista:                   
+                        if(item != producto and SM(None, item.get('title'), producto.get('title')).ratio() >= 0.6):
+                            resultado.append(producto)
+                            resultadoYaestan.append(producto.get('title'))
+                    if(resultado.count > 0):
+                        resultado.append(item)
+                        self.guardarTablaHtmlComparativa(resultado,item.get('title'))
+                    resultado.clear()    
         elif(tipo == "b"):
-            elementos = self._miLista.groupby(['title']).mean()
-            resultado = elementos
+            for item in self._miLista:
+                if(item.get('title') not in resultadoYaestan):
+                    suma = 0
+                    for producto in self._miLista:                   
+                        if(item != producto and SM(None, item.get('title'), producto.get('title')).ratio() >= 0.6):
+                            suma += producto.get('price')
+                            resultado.append(producto)
+                            resultadoYaestan.append(producto.get('title'))
+                    cantidadItems = resultado.count
+                    if(cantidadItems > 0):
+                        suma += item.get('price')
+                        aGuardar = ItemsAuxiliar(item.get('title'),item.get('categoria'),item.get('price'),item.get('link'),item.get('fecha'),item.get('market'),suma/cantidadItems,cantidadItems)
+                        resultado.append(aGuardar)
+            self.guardarTablaHtmlComparativa(resultado,"ComparativaOrdenada")
+            resultado.clear()    
         else:
-            elementos = self.contarElementosLista(self._miLista)
-            for k, v in elementos.items():
-                if(v == 1):
-                    resultado.append(k)                    
-        return resultado
+            for item in self._miLista:
+                for producto in self._miLista:                   
+                    if(item != producto and SM(None, item.get('title'), producto.get('title')).ratio() >= 0.6):
+                        resultado.append(producto)
+                    if(resultado.count == 0):
+                        resultado.append(item)
+            self.guardarTablaHtmlComparativa(resultado,"Unicos")                   
 
     def estadisticas(self,tipo):
             tienda1 = []
@@ -105,3 +197,27 @@ class Procesador:
                     tienda3.append(item) 
                 else:
                     tienda4.append(item)
+
+
+
+
+
+s1 = 'Celular Libre Samsung Galaxy S20 Ultra Gris'
+s2 = 'CELULAR LIBRE SAMSUNG S20 ULTRA GRIS'
+print(SM(None, s1.lower(), s2.lower()).ratio())
+
+
+lista1 = ["Celular Libre Samsung Galaxy S20 Ultra Gris"]
+lista2 = ["Celular Libre Samsung S20 6.2 8/128 Gris",
+          "CELULAR LIBRE SAMSUNG S20 ULTRA GRIS",
+          "CELULAR SAMSUNG GALAXY S20+ GRIS",
+          "CELULAR SAMSUNG GALAXY S20 AZUL",
+          "Funda Samsung LED Back Cover S10 White",
+          "Celular Libre Samsung S10 ",]
+
+d = difflib.Differ()
+
+for item in lista1:
+    for producto in lista2:                   
+        if(item != producto and SM(None, item.lower(), producto.lower()).ratio() >= 0.6):
+            print(producto)
