72e6fd118956d15640579af7fc5ff35361a6e4d5
Liber Rossetti <liber.rossetti@ayi-asociados.com>
2020-11-15 17:29:07 -0300

    ordeno test

--- test_yarara.py
+++ test_yarara.py
@@ -4,6 +4,10 @@ from os import path
 import pathlib
 import datetime
 from config import Config
+import menu
+from menu import QuiereSalirException
+import unittest
+from unittest.mock import patch
 
 utils = Utils()
 def test_tipo_busqueda_1():
@@ -49,17 +53,17 @@ criterio = 'resultados test'.replace(' ','_')
 procesador = Procesador(lista, criterio)
 
 def test_imprimir_archivo_csv():
-    procesador.ImprimirArchivo('csv')
+    procesador.imprimirArchivo('csv')
     filename = criterio + "_" + datetime.datetime.now().strftime("%d-%m-%Y") + '.csv'
     assert path.isfile('resultados/' + filename)
 
 def test_imprimir_archivo_json():
-    procesador.ImprimirArchivo('json')
+    procesador.imprimirArchivo('json')
     filename = criterio + "_" + datetime.datetime.now().strftime("%d-%m-%Y") + '.json'
     assert path.isfile('resultados/' + filename)
 
 def test_imprimir_archivo_html():
-    procesador.ImprimirArchivo('html')
+    procesador.imprimirArchivo('html')
     filename = criterio + "_" + datetime.datetime.now().strftime("%d-%m-%Y") + '.html'
     assert path.isfile('resultados/' + filename)
 
@@ -75,3 +79,57 @@ def test_config_tipos():
 
 def test_config_url():
     assert config.get_start_url()['fravega'] == 'https://www.fravega.com/'
+
+#Test de Menu
+class TestMenu(unittest.TestCase):
+    
+    menu_t = menu.Menu()
+    
+    # Test para formato '1'
+    @patch("builtins.input", return_value="1")
+    def test_pedir_formato_1(self, mock_input):
+        self.assertEqual(self.menu_t._Menu__pedir_formato(), "1")
+        
+    # Test para input '0' (Quiere salir)
+    @patch("builtins.input", return_value="0")
+    def test_pedir_formato_0(self, mock_input):
+        with self.assertRaises(QuiereSalirException):
+            self.menu_t._Menu__pedir_formato()
+            
+    # Test para formato pedir pagina
+    @patch("builtins.input", return_value="3")
+    def test_pedir_pagina_3(self, mock_input):
+        self.assertEqual(self.menu_t._Menu__pedir_pagina(), ["3"])
+        
+    # Test para formato pedir varias paginas
+    @patch("builtins.input", return_value="3,1,2")
+    def test_pedir_paginas(self, mock_input):
+        self.assertEqual(self.menu_t._Menu__pedir_pagina(), ["3", '1', '2'])
+        
+    # Test para input '0' (Quiere salir)
+    @patch("builtins.input", return_value="0")
+    def test_pedir_pagina_0(self, mock_input):
+        with self.assertRaises(QuiereSalirException):
+            self.menu_t._Menu__pedir_pagina()
+    
+    # Test para formato pedir tipo busqueda
+    @patch("builtins.input", return_value="2")
+    def test_pedir_tipo_busqueda(self, mock_input):
+        self.assertEqual(self.menu_t._Menu__tipo_busqueda(), '2')
+        
+    # Test para input '0' (Quiere salir)
+    @patch("builtins.input", return_value="0")
+    def test_pedir_tipo_busqueda_0(self, mock_input):
+        with self.assertRaises(QuiereSalirException):
+            self.menu_t._Menu__tipo_busqueda()
+            
+    # Test para formato pedir extras
+    @patch("builtins.input", return_value="2")
+    def test_pedir_extra_2(self, mock_input):
+        self.assertEqual(self.menu_t._Menu__pedir_extras(), '2')
+        
+    # Test para input '0' (Quiere salir)
+    @patch("builtins.input", return_value="0")
+    def test_pedir_extra_0(self, mock_input):
+        with self.assertRaises(QuiereSalirException):
+            self.menu_t._Menu__pedir_extras()
